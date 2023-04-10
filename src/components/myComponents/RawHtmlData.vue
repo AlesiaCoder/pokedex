6318b9b366fdccfecdeac27bef2da54ecba1e779
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-04-10 07:15:40 +0200

    v-if and v-show

--- src/components/myComponents/RawHtmlData.vue
+++ src/components/myComponents/RawHtmlData.vue
@@ -1,15 +1,32 @@
 <script>
-export default {};
+export default {
+    data(){
+        return{
+            title: "This is my title for Monday",
+            person:{
+                id: 1,
+                name : 'Bill',
+                img : "https://www.ecured.cu/images/0/0a/Atrapado_en_el_tiempo_%28Pel%C3%ADcula%29.jpg",
+            },
+            msg: "Bill uno de los integrantes de los cazafantasmas",
+        }
+    },
+};
 </script>
 
+
 <template>
-    <div>
-        <h3>Pikachuuuu</h3>
+    <h3>{{ title }}</h3>
+    <div :id="person.id"> 
+        <h4>{{ person.name }}</h4>
+        <img :src="person.img" :alt="person.name">
     </div>
+    <h5 v-html="msg"></h5>
 </template>
 
+
 <style lang="css" scoped>
-    h3{
-    color:aqua;
-    }
+h3 {
+    color:magenta;
+}
 </style>
--- src/views/MyView.vue
+++ src/views/MyView.vue
@@ -1,20 +1,25 @@
 <script>
+import DirectiveVShow from '../components/myComponents/directives/DirectiveVShow.vue';
+import DirectiveVif from '../components/myComponents/directives/DirectiveVif.vue';
 import SingleComponent from '../components/myComponents/SingleComponent.vue';
 import RawHtmlData from '../components/myComponents/RawHtmlData.vue';
 export default {
-  components: { SingleComponent, RawHtmlData },};
+  components: { SingleComponent, RawHtmlData, DirectiveVShow, DirectiveVif },};
 </script>
 
 <template>
-<div>
-    <h3>My Single Page</h3>
-    <single-component></single-component>
-    <single-component></single-component>
-    <single-component></single-component>
-    <single-component></single-component>
-    <raw-html-data></raw-html-data>
-
-</div>
+    <div>
+        <h1>My Single Page from FemCoder</h1>
+        <h3>Lorem ipsum dolor, sit amet consectetur...</h3>
+        ----------------------------------
+        <single-component></single-component>
+        ----------------------------------
+        <raw-html-data></raw-html-data>
+        ----------------------------------
+        <directive-v-show></directive-v-show>
+        ----------------------------------
+        <directive-vif></directive-vif>
+    </div>
 </template>
 
 <style lang="css" scoped>
